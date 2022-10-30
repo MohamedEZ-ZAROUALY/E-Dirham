3324fe2c87248485ee54ff28d09ae791f53033ea
Mohamed <ezzaroualymuhammad@gmail.com>
2022-10-30 20:35:51 +0100

    E-Dirham first deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,10 +5,6 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.17",
   networks: {
-    rinkeby: {
-      url: process.env.INFURA_RINKEBY_ENDPOINT,
-      accounts: [process.env.PRIVATE_KEY]
-    },
     goerli: {
       url: process.env.INFURA_GOERLI_ENDPOINT,
       accounts: [process.env.PRIVATE_KEY]
